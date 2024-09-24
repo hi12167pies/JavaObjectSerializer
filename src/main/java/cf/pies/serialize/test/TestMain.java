8ab08de04d842fd1b8aa33bfee4172d231878d85
hi12167pies <74526133+hi12167pies@users.noreply.github.com>
2024-09-24 22:03:18 +1000

    remove unused import

--- src/main/java/cf/pies/serialize/test/TestMain.java
+++ src/main/java/cf/pies/serialize/test/TestMain.java
@@ -3,8 +3,6 @@ package cf.pies.serialize.test;
 import cf.pies.serialize.Deserializer;
 import cf.pies.serialize.Serializer;
 
-import java.io.IOException;
-
 public class TestMain {
     public static void main(String[] args) throws Exception {
         TestUser user = new TestUser("John", 15);
